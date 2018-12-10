4c5577c2c0fd3761808625d461186b6884cf4fda
harusoraumemomosakura <harusoraumemomosakura@gmail.com>
2018-12-10 07:28:00 +0000

    have

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -14,28 +14,33 @@ class ItemsController < ApplicationController
       })
 
       results.each do |result|
-         # 扱い易いように Item としてインスタンスを作成する（保存はしない）
-        item = Item.new(read(result))
+        item = Item.find_or_initialize_by(read(result))
         @items << item  #item を [] に追加
       end
     end
   end
+  
+  def show
+    @item = Item.find(params[:id])
+    @want_users = @item.want_users
+    @have_users = @item.have_users
+  end
 
-  private
+  #private
 
-  def read(result)  #必要な値を読み出して、最後にハッシュとして return
-    code = result['itemCode']
-    name = result['itemName']
-    url = result['itemUrl']
-    image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
+  #def read(result)  #必要な値を読み出して、最後にハッシュとして return
+  #  code = result['itemCode']
+  #  name = result['itemName']
+  #  url = result['itemUrl']
+  #  image_url = result['mediumImageUrls'].first['imageUrl'].gsub('?_ex=128x128', '')
       #第一引数を見つけ出して、第二引数に置換するメソッド⇒第ニ引数に '' とカラ文字を入れているので、見つけたら削除する
       #画像 URL 末尾に含まれる ?_ex=128x128 を削除
 
-    {
-      code: code,
-      name: name,
-      url: url,
-      image_url: image_url,
-    }
-  end
+  #  {
+  #    code: code,
+  #    name: name,
+  #    url: url,
+  #    image_url: image_url,
+  #  }
+  #end
 end
